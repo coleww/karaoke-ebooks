f955dfda29969f7f46d7e179ebaeccd8ec725afe
Cole Willsea <coleww@gmail.com>
2015-04-09 19:15:42 -0700

    extract drum sampler logic

---
+++ drum.js
@@ -0,0 +1,39 @@
+var Drum = function(sampler, name){
+  this.sampler = sampler;
+  this.name = name;
+  this.notes = Array(16);
+  this.installRow();
+}
+
+Drum.prototype.play = function(pos){
+  if(Math.random() < this.notes[pos]){
+    this.sampler.start();
+  }
+}
+
+Drum.prototype.installRow = function(){
+  var that = this;
+
+  var drum = document.createElement("div")
+  drum.setAttribute("class", name);
+
+  for(var i = 0; i < this.notes.length; i++){
+    var cell = document.createElement("input");
+    cell.setAttribute("type", "text")
+    cell.setAttribute("data-index", i);
+    cell.addEventListener('keyup', function updateProbz(e){
+      that.notes[~~e.target.dataset.index] = parseFloat(e.target.value);
+    });
+    drum.appendChild(cell);
+  }
+
+  document.body.appendChild(drum);
+}
+
+Drum.prototype.loadRow = function(notes){
+  // notes => array of values
+  // replaces this.notes
+  // update dom
+}
+
+module.exports = Drum;
--- index.js
+++ index.js
@@ -1,46 +1,27 @@
 // var int2freq = require('int2freq');
 
 var Sampler = require('./sampler');
+var Drum = require('./drum');
+
 var ac = new AudioContext();
 
 var player = new Sampler(ac, 'samples/clap.wav');
-
 player.connect(ac.destination);
-
-
-
-
-
-
-
-
+var instrument = new Drum(player, 'clap');
 
 var position = 0;
-var notes = [0, 0, 0, 0];
-var inputs = document.querySelectorAll('input[type="text"]')
-
-for(var i=0;i<inputs.length;i++){
-  inputs[i].addEventListener('keyup', function updateProbz(e){
-    notes[~~e.target.dataset.index] = parseFloat(e.target.value);
-  });
-}
-
-
-
-
-
 
 function run(){
   window.setInterval(function(){
     console.log("BONG")
-    if(Math.random() < notes[position]){
-      player.start();
-    }
+
+    instrument.play(position);
+
     position++;
-    if(position > 3){
+    if(position >= 16){
       position = 0;
     }
   }, 500)
 }
 
-  run();
+run();
