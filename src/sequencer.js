cf3f0651669dcd9727b8bdab994cbb04c97572be
Cole Willsea <coleww@gmail.com>
2015-09-06 15:51:29 -0700

    down this road again

--- src/sequencer.js
+++ src/sequencer.js
@@ -13,7 +13,7 @@ var Sequencer = function(data, lines){
   this.lines = lines
   console.log('SNOW: ', this.lines.length)
   console.log(this.lines)
-  this.section = data.section || "verse"
+  this.section = data.section || "intro"
   this.sections = data.sections || ["verse", "verse", "verse", "verse", "chorus", "chorus"]
   this.position = 0;
   this.steps = data.steps;
