7dc52acebd5d86a653fac13abcb68f09d8a4bb2e
Cole Willsea <coleww@gmail.com>
2015-10-14 07:05:32 -0700

    nice, simple, cool

--- index.js
+++ index.js
@@ -1,23 +1,10 @@
-console.log("v1.1.1")
-
-
+console.log("v1.1.2")
 var data50s = require('./songs/data');
-var data80s = require('./songs/blue_mondata');
 var xhr = require('xhr')
 var constructForMeASongPlease = require('./rhymeSrc/makeASong')
 var loadSample2Buff = require('load-sample-2-buff')
 var SamplePlayer = require('openmusic-sample-player')
-
-var flavorStash = {
-  'marx': require('./flavors/marx'),
-  'smiths': require('./flavors/smiths'),
-  'oldies': require('./flavors/oldies'),
-  'punk': require('./flavors/punk'),
-  'taylor': require('./flavors/taylor'),
-}
-
 var Sequencer = require('./audioSrc/sequencer');
-
 var host = 'http://karaoke-ebooks.herokuapp.com' //
 // var host = 'http://localhost:8000'
 
@@ -31,28 +18,20 @@ document.getElementById('doit').addEventListener('click', function (){
       throw err
     } else {
       var linesToAdd = JSON.parse(body).data
-      var flavors = document.querySelectorAll('input[name=flavors]:checked');
-      for (var i = 0; i < flavors.length; i++){
-        console.log(flavors[i].value)
-        linesToAdd = linesToAdd.concat(flavorStash[flavors[i].value])
-      }
       var now = new Date().getTime()
       constructForMeASongPlease(linesToAdd, function (lines) {
         var then = new Date().getTime()
         console.log('WOW that only took ', then - now)
         document.getElementById('loading').style.display = "none"
-        var stype = document.querySelector('input[name="songtype"]:checked').value
 
-        var data = (stype == '50s') ? data50s : data80s
+        var data = data50s
 
-        data.key.tonic = document.querySelectorAll('#opts select')[0].value + '2'
-        data.key.scale = document.querySelectorAll('#opts select')[1].value
+        data.key.tonic = 'C2'
+        data.key.scale = 'major'
         var seq = new Sequencer(data, lines);
         seq.run()
         document.getElementById('bruh').style.display = "inline-block"
         document.getElementById('karaoke').style.display = "inline-block"
-
-
       })
     }
   })
